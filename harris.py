31cdb367a7ee3572babf17538cdef811f643c424
Guilherme Polo <ggpolo@gmail.com>
2011-04-17 01:24:26 -0300

    Split GUI from the actual detector.

---
+++ harris.py
@@ -0,0 +1,233 @@
+# -*- encoding: utf8 -*-
+import math
+import numpy
+from scipy import signal
+from PIL import Image, ImageDraw, ImageOps, ImageTk
+from collections import defaultdict
+
+
+def dist_l2(p1, p2):
+    return sum([(x1 - x2) ** 2 for x1, x2 in zip(p1, p2)]) ** (1/2.)
+
+def gaussian_mask(width, height, sigma):
+    u = (width - 1) / 2
+    v = (height - 1) / 2
+
+    w = []
+    for i in xrange(-u, u + 1):
+        w.append([math.exp(-(i**2 + j**2)/(2*(sigma**2)))
+            for j in xrange(-v, v + 1)])
+    return w
+
+def flood_fill(pix, max_x, max_y, color=128):
+    to_do = [(0, 0)]
+    while to_do:
+        x, y = to_do.pop()
+        pix[x, y] = color
+        if x > 0:
+            if not pix[x-1, y]: to_do.append((x-1, y))
+        if x < max_x - 1:
+            if not pix[x+1, y]: to_do.append((x+1, y))
+        if y < max_y - 1:
+            if not pix[x, y+1]: to_do.append((x, y+1))
+        if y > 0:
+            if not pix[x, y-1]: to_do.append((x, y-1))
+
+
+def harris(img, threshold, sigma=0.5, wwidth=3, wheight=3, invert=False):
+    if len(img.getbands()) == 1:
+        img = img.convert('RGB')
+    if invert:
+        img = ImageOps.invert(img)
+
+    new_img = img.copy()
+    img_pix = new_img.load()
+
+    raw_harris_img = Image.new('RGB', img.size)
+    edge_img = Image.new('L', img.size)
+    edge_pix = edge_img.load()
+    corner_img = Image.new('L', img.size)
+    corner_pix = corner_img.load()
+    caption_reg_img = Image.new('L', img.size)
+    caption_pix = caption_reg_img.load()
+
+    w = gaussian_mask(wwidth, wheight, sigma)
+
+    #band = list(img.split()) + [ImageOps.grayscale(img)]
+    band = [ImageOps.grayscale(img)]
+    #band = list(img.split())
+    for img_gray in band:
+        do_harris(new_img, img_gray, raw_harris_img, edge_pix, edge_img,
+                corner_pix, threshold, w)
+
+    max_x, max_y = new_img.size
+
+    new_white = []
+    for x in xrange(max_x):
+        for y in xrange(max_y):
+            if edge_pix[x, y]:
+                continue
+            try:
+                if edge_pix[x-1, y] or edge_pix[x+1, y]:
+                    new_white.append((x, y))
+                elif edge_pix[x, y-1] or edge_pix[x, y+1]:
+                    new_white.append((x, y))
+            except IndexError:
+                pass
+    for x, y in new_white:
+        edge_pix[x, y] = 255
+
+    flood_fill(edge_pix, max_x, max_y, color=128)
+
+    for x in xrange(max_x):
+        for y in xrange(max_y):
+            if edge_pix[x, y] or caption_pix[x, y]:
+                continue
+            caption_pix[x, y] = 255
+            #x_orig, y_orig = x, y
+            #while y - 1 > 0 and edge_pix[x, y] in (255, 0):
+            while y - 1 > 0 and not edge_pix[x, y]:
+                y -= 1
+                caption_pix[x, y] = 255
+            #x, y = x_orig, y_orig
+            #while y + 1 < max_y and edge_pix[x, y] in (255, 0):
+            while y + 1 < max_y and not edge_pix[x, y]:
+                y += 1
+                caption_pix[x, y] = 255
+            #x, y = x_orig, y_orig
+            #while x + 1 < max_x and edge_pix[x, y] in (255, 0):
+            while x + 1 < max_x and not edge_pix[x, y]:
+                x += 1
+                caption_pix[x, y] = 255
+            #x, y = x_orig, y_orig
+            #while x - 1 > 0 and edge_pix[x, y] in (255, 0):
+            while x - 1 > 0 and not edge_pix[x, y]:
+                x -= 1
+                caption_pix[x, y] = 255
+
+    data = list(caption_reg_img.getdata())
+    # Median filter
+    masksize = 5
+    maskelems = masksize ** 2
+    start = (masksize - 1) / 2
+
+    for x in xrange(start, max_x - start):
+        for y in xrange(start, max_y - start):
+            val = [caption_pix[x+i, y+j]
+                for i in xrange(-start, start + 1)
+                for j in xrange(-start, start + 1)]
+            val.sort()
+            data[y * max_x + x] = val[maskelems / 2]
+    caption_reg_img.putdata(data)
+
+
+    return w, new_img, raw_harris_img, edge_img, corner_img, caption_reg_img
+
+
+def do_harris(new_img, img_gray, raw_harris_img, edge_pix, edge_img,
+        corner_pix, threshold, w):
+    img_orig = new_img.copy()
+    img_pix = new_img.load()
+    img2d = numpy.asarray(img_gray)
+
+    X = signal.convolve2d(img2d, [[-1, 0, 1]], mode='same')
+    Y = signal.convolve2d(img2d, [[-1], [0], [1]], mode='same')
+
+    A = signal.convolve2d(X ** 2, w, mode='same')
+    B = signal.convolve2d(Y ** 2, w, mode='same')
+    C = signal.convolve2d(X * Y, w, mode='same')
+
+    Tr = A + B
+    Det = A * B - (C ** 2)
+    k = 0.05
+    R = Det - k * (Tr ** 2)
+
+    print '-+' * 30
+
+    max_x, max_y = new_img.size
+
+    # Corner points
+    corner = []
+    max_interest = numpy.max(R)
+    discard = (max_interest / 100.) * threshold
+
+    # Hysteresis on edges
+    edge = set()#[]
+    maybe_edge = defaultdict(int)
+    max_edge = abs(numpy.min(R[R<0]))
+    #min_edge = abs(numpy.max(R[R<0]))
+    #avg_edge = abs(numpy.mean(R[R<0]))
+    #stddev_edge = numpy.std(R[R<0])
+    #median_edge = abs(numpy.median(R[R<0]))
+    upper_threshold = max_edge - (max_edge / 100. * 99)
+    lower_threshold = max_edge - (max_edge / 100. * 99.999)
+    print "Hysteresis thresholds: %.2f %.2f" % (upper_threshold,
+            lower_threshold)
+
+    xstart = (len(w[0]) + 1) / 2
+    ystart = (len(w) + 1) / 2
+
+    raw_pix = raw_harris_img.load()
+
+    for i in xrange(xstart, max_x - xstart):
+        for j in xrange(ystart, max_y - ystart):
+            # Corner pixel:
+            # Check if R[j][i] is an 8-way local maximum
+            if R[j, i] > 0:
+                if R[j, i] == numpy.max(R[j-1:j+2, i-1:i+2]):
+                    raw_pix[i, j] = (255, 0, 0)
+                    if R[j, i] - discard > 0:
+                        corner.append([i, j])
+
+            # Edge pixel
+            elif R[j, i] < 0:
+                test_edge = None
+                # XXX Descobri que isso é chamado de "Non-Maximum Suppression"
+                if A[j, i] > B[j, i]:
+                    if R[j, i] < min([R[j, i - 1], R[j, i + 1]]):
+                        test_edge = (i, j, abs(R[j, i]))
+                        raw_pix[i, j] = (0, 0, 255)
+                else:
+                    if R[j][i] < min([R[j - 1, i], R[j + 1, i]]):
+                        test_edge = (i, j, abs(R[j, i]))
+                        raw_pix[i, j] = (0, 0, 255)
+
+                if test_edge is not None:
+                    #img_pix[test_edge[0], test_edge[1]] = (0, 0, 255)
+                    #edge_pix[test_edge[0], test_edge[1]] = 255
+                    if test_edge[2] > upper_threshold:
+                        #edge.append([test_edge[0], test_edge[1]])
+                        edge.add((test_edge[0], test_edge[1]))
+                    elif test_edge[2] > lower_threshold:
+                        maybe_edge[(test_edge[0], test_edge[1])] = 1
+
+    # Mark and draw corner points.
+    draw = ImageDraw.Draw(new_img)
+    for (i, j) in corner:
+        corner_pix[i, j] = 255
+        draw.rectangle((i-1, j-1, i+1, j+1), fill='red')
+
+    nhood = 1
+    dirs = [(x, y) for x in xrange(-nhood, nhood + 1)
+       for y in xrange(-nhood, nhood + 1) if x != 0 and y != 0]
+    do_hysteresis(img_pix, edge_pix, edge, maybe_edge, lower_threshold, dirs)
+
+    draw = ImageDraw.Draw(edge_img)
+    for x, y in corner:
+        draw.rectangle((x-1, y-1, x+1, y+1), fill='white')
+
+    #return R
+
+
+# XXX Testing hysteresis
+def do_hysteresis(img_pix, edge_pix, edge, maybe_edge, lower_threshold, dirs):
+    while edge:
+        i, j = edge.pop()
+        img_pix[i, j] = (0, 255, 255)
+        edge_pix[i, j] = 255
+        for x, y in dirs:
+            try:
+                if (maybe_edge[i+x, j+y] and not edge_pix[i+x, j+y]):
+                    edge.add((i+x, j+y))
+            except IndexError:
+                pass
--- harris_pil.py
+++ harris_pil.py
@@ -1,15 +1,9 @@
 # -*- encoding: utf8 -*-
 import os
 import sys
-import math
-import time
-import numpy
 import Tkinter
 import tkFileDialog
-from scipy import signal
-from PIL import Image, ImageDraw, ImageOps, ImageTk
-from colorsys import rgb_to_hsv
-from collections import defaultdict
+from PIL import Image, ImageTk
 
 #import matplotlib
 #matplotlib.use('TkAgg') # XXX Esse backend deixa mais lento o plot,
@@ -18,238 +12,14 @@ from collections import defaultdict
 #from matplotlib import pyplot
 #from matplotlib.colors import BoundaryNorm
 
-def dist_l2(p1, p2):
-    return sum([(x1 - x2) ** 2 for x1, x2 in zip(p1, p2)]) ** (1/2.)
-
-def gaussian_mask(width, height, sigma):
-    u = (width - 1) / 2
-    v = (height - 1) / 2
-
-    w = []
-    for i in xrange(-u, u + 1):
-        w.append([math.exp(-(i**2 + j**2)/(2*(sigma**2)))
-            for j in xrange(-v, v + 1)])
-    return w
-
-def harris(img, threshold, sigma=0.5, wwidth=3, wheight=3, invert=False):
-    if len(img.getbands()) == 1:
-        img = img.convert('RGB')
-    if invert:
-        img = ImageOps.invert(img)
-
-    new_img = img.copy()
-    img_pix = new_img.load()
-
-    edge_img = Image.new('L', img.size)
-    edge_pix = edge_img.load()
-    corner_img = Image.new('L', img.size)
-    corner_pix = corner_img.load()
-    caption_reg_img = Image.new('L', img.size)
-    caption_pix = caption_reg_img.load()
-
-    w = gaussian_mask(wwidth, wheight, sigma)
-
-    #band = list(img.split()) + [ImageOps.grayscale(img)]
-    #band = [ImageOps.grayscale(img)]
-    band = list(img.split())
-    for img_gray in band:
-        do_harris(new_img, img_gray, edge_pix, edge_img, corner_pix,
-                threshold, w)
-
-    max_x, max_y = new_img.size
-
-    new_white = []
-    for x in xrange(max_x):
-        for y in xrange(max_y):
-            if edge_pix[x, y]:
-                continue
-            try:
-                if edge_pix[x-1, y] or edge_pix[x+1, y]:
-                    new_white.append((x, y))
-                elif edge_pix[x, y-1] or edge_pix[x, y+1]:
-                    new_white.append((x, y))
-            except IndexError:
-                pass
-    for x, y in new_white:
-        edge_pix[x, y] = 255
-
-    flood_fill(edge_pix, max_x, max_y)
-
-    for x in xrange(max_x):
-        for y in xrange(max_y):
-            if edge_pix[x, y] or caption_pix[x, y]:
-                continue
-            caption_pix[x, y] = 255
-            while y - 1 > 0 and not not edge_pix[x, y]:# in (255, 0):
-                y -= 1
-                caption_pix[x, y] = 255
-            while y + 1 < max_y and not edge_pix[x, y]:# in (255, 0):
-                y += 1
-                caption_pix[x, y] = 255
-            while x + 1 < max_x and not edge_pix[x, y]:# in (255, 0):
-                x += 1
-                caption_pix[x, y] = 255
-            while x - 1 > 0 and not edge_pix[x, y]:# in (255, 0):
-                x -= 1
-                caption_pix[x, y] = 255
-
-    data = list(caption_reg_img.getdata())
-    masksize = 3
-    maskelems = masksize ** 2
-    start = (masksize - 1) / 2
-
-    for x in xrange(start, max_x - start):
-        for y in xrange(start, max_y - start):
-            val = [caption_pix[x+i, y+j]
-                for i in xrange(-start, start + 1)
-                for j in xrange(-start, start + 1)]
-            val.sort()
-            data[y * max_x + x] = val[maskelems / 2]
-    caption_reg_img.putdata(data)
+from harris import harris
 
 
-    return w, new_img, edge_img, corner_img, caption_reg_img
-
-
-def do_harris(new_img, img_gray, edge_pix, edge_img, corner_pix, threshold, w):
-    img_orig = new_img.copy()
-    img_pix = new_img.load()
-    img2d = numpy.asarray(img_gray)
-
-    X = signal.convolve2d(img2d, [[-1, 0, 1]], mode='same')
-    Y = signal.convolve2d(img2d, [[-1], [0], [1]], mode='same')
-
-    A = signal.convolve2d(X ** 2, w, mode='same')
-    B = signal.convolve2d(Y ** 2, w, mode='same')
-    C = signal.convolve2d(X * Y, w, mode='same')
-
-    Tr = A + B
-    Det = A * B - (C ** 2)
-    k = 0.05
-    R = Det - k * (Tr ** 2)
-
-    print '-+' * 30
-
-    max_x, max_y = new_img.size
-
-    # Corner points
-    corner = []
-    max_interest = numpy.max(R)
-    discard = (max_interest / 100.) * threshold
-
-    # Hysteresis on edges
-    edge = set()#[]
-    maybe_edge = defaultdict(int)
-    max_edge = abs(numpy.min(R[R<0]))
-    #min_edge = abs(numpy.max(R[R<0]))
-    #avg_edge = abs(numpy.mean(R[R<0]))
-    #stddev_edge = numpy.std(R[R<0])
-    #median_edge = abs(numpy.median(R[R<0]))
-    upper_threshold = max_edge - (max_edge / 100. * 99)
-    lower_threshold = max_edge - (max_edge / 100. * 99.999)
-    print "Hysteresis thresholds: %.2f %.2f" % (upper_threshold,
-            lower_threshold)
-
-    xstart = (len(w[0]) + 1) / 2
-    ystart = (len(w) + 1) / 2
-
-    for i in xrange(xstart, max_x - xstart):
-        for j in xrange(ystart, max_y - ystart):
-            # Corner pixel:
-            # Check if R[j][i] is an 8-way local maximum
-            if R[j, i] > 0 and R[j, i] == numpy.max(R[j-1:j+2, i-1:i+2]):
-                if R[j, i] - discard > 0:
-                    corner.append([i, j])
-
-            # Edge pixel
-            elif R[j, i] < 0:
-                test_edge = None
-                # XXX Descobri que isso é chamado de "Non-Maximum Suppression"
-                if A[j, i] > B[j, i]:
-                    if R[j, i] < min([R[j, i - 1], R[j, i + 1]]):
-                        test_edge = (i, j, abs(R[j, i]))
-                else:
-                    if R[j][i] < min([R[j - 1, i], R[j + 1, i]]):
-                        test_edge = (i, j, abs(R[j, i]))
-
-                if test_edge is not None:
-                    #img_pix[test_edge[0], test_edge[1]] = (0, 0, 255)
-                    #edge_pix[test_edge[0], test_edge[1]] = 255
-                    if test_edge[2] > upper_threshold:
-                        #edge.append([test_edge[0], test_edge[1]])
-                        edge.add((test_edge[0], test_edge[1]))
-                    elif test_edge[2] > lower_threshold:
-                        maybe_edge[(test_edge[0], test_edge[1])] = 1
-
-    # Mark and draw corner points.
-    draw = ImageDraw.Draw(new_img)
-    for (i, j) in corner:
-        corner_pix[i, j] = 255
-        draw.rectangle((i-1, j-1, i+1, j+1), fill='red')
-
-    nhood = 1
-    dirs = [(x, y) for x in xrange(-nhood, nhood + 1)
-       for y in xrange(-nhood, nhood + 1) if x != 0 and y != 0]
-    do_hysteresis(img_pix, edge_pix, edge, maybe_edge, lower_threshold, dirs)
-
-    draw = ImageDraw.Draw(edge_img)
-    for x, y in corner:
-        draw.rectangle((x-1, y-1, x+1, y+1), fill='white')
-
-    max_x, max_y = img_orig.size
-
-
-    #print R
-    # XXX Trying to display R as a heatmap
-    #f = pyplot.figure(figsize=(4, 3), frameon=False)
-
-    #bdata = list(numpy.linspace(R.min(), numpy.max(R[R<0]), 12)) + [0]
-    #bdata += list(numpy.linspace(numpy.min(R[R>0]), R.max(), 12))
-
-    #ax = f.add_axes([0.01, 0.01, 0.87, 0.95])
-    #ax.set_axis_off()
-
-    #im = ax.matshow(R, norm=BoundaryNorm(bdata, ncolors=256, clip=False),
-    #        cmap=pyplot.cm.gnuplot2)
-    #f.colorbar(im, shrink=0.89, format='%0.2g')
-
-    #f.show()
-
-    #from matplotlib.backends.backend_pdf import PdfPages
-    #pp = PdfPages('plot1.pdf')
-    #pp.savefig()
-    #pp.close()
-
-
-def flood_fill(edge_pix, max_x, max_y):
-    to_do = [(0, 0)]
-    while to_do:
-        x, y = to_do.pop()
-        edge_pix[x, y] = 128
-        if x > 0:
-            if not edge_pix[x-1, y]: to_do.append((x-1, y))
-        if x < max_x - 1:
-            if not edge_pix[x+1, y]: to_do.append((x+1, y))
-        if y < max_y - 1:
-            if not edge_pix[x, y+1]: to_do.append((x, y+1))
-        if y > 0:
-            if not edge_pix[x, y-1]: to_do.append((x, y-1))
-
-
-# XXX Testing hysteresis
-def do_hysteresis(img_pix, edge_pix, edge, maybe_edge, lower_threshold, dirs):
-    while edge:
-        i, j = edge.pop()
-        img_pix[i, j] = (0, 255, 255)
-        edge_pix[i, j] = 255
-        for x, y in dirs:
-            try:
-                if (maybe_edge[i+x, j+y] and not edge_pix[i+x, j+y]):
-                    edge.add((i+x, j+y))
-            except IndexError:
-                pass
-
-
+def withdraw(event):
+    widget = root.nametowidget(event.widget)
+    while not hasattr(widget, 'withdraw'):
+        widget = widget.master
+    widget.withdraw()
 
 def scrolledcanvas(root, master, **kwargs):
     canvas = Tkinter.Canvas(master, **kwargs)
@@ -285,7 +55,7 @@ class App(object):
         self._img_show.s_img = img
         self._img_show.img = src_img
 
-    def _pre_detector(self, event=None):
+    def _run_detector(self, event=None):
         try:
             threshold = float(self._threshold.get())
             sigma = float(self._sigma.get())
@@ -299,29 +69,27 @@ class App(object):
 
         invert_img = self._invert.get()
 
-        start = time.time()
         images = harris(self._img_show.img, threshold, sigma, wwidth, wheight,
                 invert=invert_img)
         mask, new_img = images[0], images[1]
-        #print time.time() - start
+
         if new_img is None:
             return
-        new_img = ImageTk.PhotoImage(new_img)
+        new_img_tk = ImageTk.PhotoImage(new_img)
         mimg = Image.new('L', (len(mask), len(mask[0])))
-        mpix = mimg.load()
-        for i in xrange(len(mask)):
-            for j in xrange(len(mask[0])):
-                mpix[i, j] = 255 * mask[i][j]
+        mimg.putdata([255 * mask[i][j]
+            for i in xrange(len(mask)) for j in xrange(len(mask[0]))])
         mimg = ImageTk.PhotoImage(mimg)
 
         window = Tkinter.Toplevel()
+        window.image = new_img
         label = Tkinter.Label(window, text='%s, %s' % (threshold, sigma))
         mask_img = Tkinter.Label(window, image=mimg)
         mask_img.img = mimg
         img_frame = Tkinter.Frame(window)
         img = scrolledcanvas(window, img_frame, highlightthickness=0)
-        canvas_addimage(img, new_img, new_img.width(), new_img.height())
-        img.img = new_img
+        img.new_img = new_img_tk
+        canvas_addimage(img, new_img_tk,new_img_tk.width(),new_img_tk.height())
         label.pack()
         mask_img.pack()
         img.pack(fill='both', expand=True)
@@ -329,7 +97,8 @@ class App(object):
 
         for image in images[2:]:
             win = Tkinter.Toplevel()
-            iimg = ImageTk.PhotoImage(image)
+            win.image = image
+            iimg = ImageTk.PhotoImage(win.image)
             iframe = Tkinter.Frame(win)
             ilbl = scrolledcanvas(win, iframe, highlightthickness=0)
             canvas_addimage(ilbl, iimg, *image.size)
@@ -338,6 +107,20 @@ class App(object):
             iframe.pack(fill='both', expand=True)
 
 
+    def _save(self):
+        try:
+            image = root.nametowidget(root.focus_get()).image
+        except AttributeError:
+            return
+        else:
+            self._save_img(image)
+
+    def _save_img(self, image):
+        name = tkFileDialog.asksaveasfilename(parent=root,
+                initialdir=os.getcwd())
+        if name is not None and len(name.strip()):
+            image.save(name)
+
     def _load_img(self):
         name = tkFileDialog.askopenfilename(initialdir=os.getcwd())
         if name is not None and len(name.strip()):
@@ -345,14 +128,24 @@ class App(object):
 
 
     def _build(self, root):
+        menu = Tkinter.Menu(name='apple')
+        file_menu = Tkinter.Menu(menu)
+        file_menu.add_command(label="Load", command=self._load_img)
+        file_menu.add_command(label="Save", command=self._save)
+        menu.add_cascade(label="Harris", menu=file_menu)
+        root.event_add('<<Close>>', '<Command-w>')
+        root.bind('<<Close>>', lambda evt: root.quit())
+        root.bind_class('all', '<<Close>>', withdraw)
+        root.config(menu=menu)
+
         ctrl_frame = Tkinter.Frame(root)
         label = Tkinter.Label(ctrl_frame, text='Threshold')
         label.focus()
         self._threshold = Tkinter.Entry(ctrl_frame, width=8)
         self._threshold.insert(0, '0.5')
         btn = Tkinter.Button(ctrl_frame, text='Apply',
-                command=self._pre_detector)
-        root.bind('<Return>', self._pre_detector)
+                command=self._run_detector)
+        root.bind('<Return>', self._run_detector)
 
         label.pack(side='left')
         self._threshold.pack(side='left')
