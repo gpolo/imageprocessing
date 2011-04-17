b148edff9b727b70e68a404b0c1996cf64ffc175
Guilherme Polo <ggpolo@gmail.com>
2011-04-17 12:08:42 -0300

    Generate an image for each step in the process. I also changed hysteresis thresholds!!

--- harris.py
+++ harris.py
@@ -43,7 +43,20 @@ def harris(img, threshold, sigma=0.5, wwidth=3, wheight=3, invert=False):
     new_img = img.copy()
     img_pix = new_img.load()
 
+    # R matrix with corner points as 8-way local maximum and
+    # non-maximum suppression.
     raw_harris_img = Image.new('RGB', img.size)
+    # Discard corner points above a threshold
+    thr1_img = Image.new('RGB', img.size)
+    # Discard edge points above an upper threshold
+    uthr_img = Image.new('RGB', img.size)
+    # Hysteresis
+    hyst_img = Image.new('RGB', img.size)
+    # Fill gaps in both x and y
+    gapsxy_img = None
+    # Caption without median filter
+    almost_there = None
+
     edge_img = Image.new('L', img.size)
     edge_pix = edge_img.load()
     corner_img = Image.new('L', img.size)
@@ -57,11 +70,12 @@ def harris(img, threshold, sigma=0.5, wwidth=3, wheight=3, invert=False):
     #band = [ImageOps.grayscale(img)]
     band = list(img.split())
     for img_gray in band:
-        do_harris(new_img, img_gray, raw_harris_img, edge_pix, edge_img,
-                corner_pix, threshold, w)
+        do_harris(new_img, img_gray, raw_harris_img, thr1_img, uthr_img,
+                hyst_img, edge_pix, edge_img, corner_pix, threshold, w)
 
     max_x, max_y = new_img.size
 
+    # Try to fill gaps both in horizontal and vertical
     new_white = []
     for x in xrange(max_x):
         for y in xrange(max_y):
@@ -76,6 +90,7 @@ def harris(img, threshold, sigma=0.5, wwidth=3, wheight=3, invert=False):
                 pass
     for x, y in new_white:
         edge_pix[x, y] = 255
+    gaps_img = edge_img.copy()
 
     flood_fill(edge_pix, max_x, max_y, color=128)
 
@@ -105,8 +120,10 @@ def harris(img, threshold, sigma=0.5, wwidth=3, wheight=3, invert=False):
                 x -= 1
                 caption_pix[x, y] = 255
 
-    data = list(caption_reg_img.getdata())
+    almost_there = caption_reg_img.copy()
+
     # Median filter
+    data = list(caption_reg_img.getdata())
     masksize = 5
     maskelems = masksize ** 2
     start = (masksize - 1) / 2
@@ -121,11 +138,14 @@ def harris(img, threshold, sigma=0.5, wwidth=3, wheight=3, invert=False):
     caption_reg_img.putdata(data)
 
 
-    return w, new_img, raw_harris_img, edge_img, corner_img, caption_reg_img
+    return (w, new_img, raw_harris_img, thr1_img, uthr_img, hyst_img,
+            gaps_img, almost_there, caption_reg_img)
+            #gaps_img, almost_there, edge_img, corner_img, caption_reg_img)
 
 
-def do_harris(new_img, img_gray, raw_harris_img, edge_pix, edge_img,
-        corner_pix, threshold, w):
+def do_harris(new_img, img_gray, raw_harris_img, thr1_img, uthr_img, hyst_img,
+        edge_pix, edge_img, corner_pix, threshold, w):
+
     img_orig = new_img.copy()
     img_pix = new_img.load()
     img2d = numpy.asarray(img_gray)
@@ -152,15 +172,15 @@ def do_harris(new_img, img_gray, raw_harris_img, edge_pix, edge_img,
     discard = (max_interest / 100.) * threshold
 
     # Hysteresis on edges
-    edge = set()#[]
+    edge = set()
     maybe_edge = defaultdict(int)
     max_edge = abs(numpy.min(R[R<0]))
     #min_edge = abs(numpy.max(R[R<0]))
     #avg_edge = abs(numpy.mean(R[R<0]))
     #stddev_edge = numpy.std(R[R<0])
     #median_edge = abs(numpy.median(R[R<0]))
-    upper_threshold = max_edge - (max_edge / 100. * 99)
-    lower_threshold = max_edge - (max_edge / 100. * 99.999)
+    upper_threshold = max_edge - (max_edge / 100. * 98)
+    lower_threshold = max_edge - (max_edge / 100. * 99.99)
     print "Hysteresis thresholds: %.2f %.2f" % (upper_threshold,
             lower_threshold)
 
@@ -168,6 +188,15 @@ def do_harris(new_img, img_gray, raw_harris_img, edge_pix, edge_img,
     ystart = (len(w) + 1) / 2
 
     raw_pix = raw_harris_img.load()
+    raw_draw = ImageDraw.Draw(raw_harris_img)
+    thr1_pix = thr1_img.load()
+    thr1_draw = ImageDraw.Draw(thr1_img)
+    uthr_pix = uthr_img.load()
+    uthr_draw = ImageDraw.Draw(uthr_img)
+    hyst_pix = hyst_img.load()
+    hyst_draw = ImageDraw.Draw(hyst_img)
+    edge_color = (0, 128, 255)
+    corner_color = (255, 255, 255)
 
     for i in xrange(xstart, max_x - xstart):
         for j in xrange(ystart, max_y - ystart):
@@ -175,7 +204,7 @@ def do_harris(new_img, img_gray, raw_harris_img, edge_pix, edge_img,
             # Check if R[j][i] is an 8-way local maximum
             if R[j, i] > 0:
                 if R[j, i] == numpy.max(R[j-1:j+2, i-1:i+2]):
-                    raw_pix[i, j] = (255, 0, 0)
+                    raw_draw.rectangle((i-1, j-1, i+1, j+1), fill=corner_color)
                     if R[j, i] - discard > 0:
                         corner.append([i, j])
 
@@ -186,11 +215,13 @@ def do_harris(new_img, img_gray, raw_harris_img, edge_pix, edge_img,
                 if A[j, i] > B[j, i]:
                     if R[j, i] < min([R[j, i - 1], R[j, i + 1]]):
                         test_edge = (i, j, abs(R[j, i]))
-                        raw_pix[i, j] = (0, 0, 255)
+                        raw_pix[i, j] = edge_color
+                        thr1_pix[i, j] = edge_color
                 else:
                     if R[j][i] < min([R[j - 1, i], R[j + 1, i]]):
                         test_edge = (i, j, abs(R[j, i]))
-                        raw_pix[i, j] = (0, 0, 255)
+                        raw_pix[i, j] = edge_color
+                        thr1_pix[i, j] = edge_color
 
                 if test_edge is not None:
                     #img_pix[test_edge[0], test_edge[1]] = (0, 0, 255)
@@ -198,6 +229,8 @@ def do_harris(new_img, img_gray, raw_harris_img, edge_pix, edge_img,
                     if test_edge[2] > upper_threshold:
                         #edge.append([test_edge[0], test_edge[1]])
                         edge.add((test_edge[0], test_edge[1]))
+                        uthr_pix[test_edge[0], test_edge[1]] = edge_color
+                        maybe_edge[(test_edge[0], test_edge[1])] = 1
                     elif test_edge[2] > lower_threshold:
                         maybe_edge[(test_edge[0], test_edge[1])] = 1
 
@@ -206,11 +239,11 @@ def do_harris(new_img, img_gray, raw_harris_img, edge_pix, edge_img,
     for (i, j) in corner:
         corner_pix[i, j] = 255
         draw.rectangle((i-1, j-1, i+1, j+1), fill='red')
+        thr1_draw.rectangle((i-1, j-1, i+1, j+1), fill=corner_color)
+        hyst_draw.rectangle((i-1, j-1, i+1, j+1), fill=corner_color)
+        uthr_draw.rectangle((i-1, j-1, i+1, j+1), fill=corner_color)
 
-    nhood = 1
-    dirs = [(x, y) for x in xrange(-nhood, nhood + 1)
-       for y in xrange(-nhood, nhood + 1) if x != 0 and y != 0]
-    do_hysteresis(img_pix, edge_pix, edge, maybe_edge, lower_threshold, dirs)
+    do_hysteresis(img_pix, edge_pix, edge, maybe_edge,lower_threshold,hyst_pix)
 
     draw = ImageDraw.Draw(edge_img)
     for x, y in corner:
@@ -220,11 +253,16 @@ def do_harris(new_img, img_gray, raw_harris_img, edge_pix, edge_img,
 
 
 # XXX Testing hysteresis
-def do_hysteresis(img_pix, edge_pix, edge, maybe_edge, lower_threshold, dirs):
+def do_hysteresis(img_pix, edge_pix, edge, maybe_edge,lower_threshold,hyst_pix):
+    nhood = 3
+    dirs = [(x, y) for x in xrange(-nhood, nhood + 1)
+       for y in xrange(-nhood, nhood + 1) if x != 0 and y != 0]
+
     while edge:
         i, j = edge.pop()
         img_pix[i, j] = (0, 255, 255)
         edge_pix[i, j] = 255
+        hyst_pix[i, j] = (0, 128, 255)
         for x, y in dirs:
             try:
                 if (maybe_edge[i+x, j+y] and not edge_pix[i+x, j+y]):
